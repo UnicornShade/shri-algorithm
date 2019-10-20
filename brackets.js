9282b137b4ab5c9f3b99939e18390fea22c0460b
Dmitry <zadima@gmail.com>
2019-10-20 13:38:09 +0300

    brackets

---
+++ brackets.js
@@ -0,0 +1,42 @@
+function bracketsBalance(expression, brackets = '()[]<>') {
+  if (typeof expression !== 'string') throw new TypeError('Expression must be a string')
+
+  const OPEN_BRACKET_MAP = {
+    '(': ')',
+    '[': ']',
+    '<': '>'
+  }
+
+  const CLOSE_BRACKET_MAP = {
+    ')': '(',
+    ']': '[',
+    '>': '<'
+  }
+
+  const stack = []
+
+  for (let char of expression) {
+    if (OPEN_BRACKET_MAP[char]) {
+      stack.push(char)
+    } else if (CLOSE_BRACKET_MAP[char]) {
+      if (OPEN_BRACKET_MAP[stack.pop()] !== char) return false
+    }
+  }
+
+  return stack.length === 0
+}
+
+const expressions = [
+  '[()[]<>()]',                              //true
+  '[(<)>]',                                  //false
+  'string without brackets',                 //true
+  '[]',                                      //true
+  '[',                                       //false
+  ')(',                                      //false
+  'correct string  [<>()] with brackets []', //true
+  'incorrect string with <[>] brackets',     //false
+  '>',                                       //false
+  ''                                         // true
+]
+
+expressions.map(expression => console.log(bracketsBalance(expression), expression))
